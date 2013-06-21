ef6c8a0d2fd814c14edd94a2f4fd7e3a183692af
ccarnero <christian.carnero@gmail.com>
2013-06-21 19:38:02 -0300

    updated tests constants credentials

--- kz.client/src/test/java/EnterpriseServiceIntegrationTest.java
+++ kz.client/src/test/java/EnterpriseServiceIntegrationTest.java
@@ -45,8 +45,8 @@ public class EnterpriseServiceIntegrationTest {
     {
         try {
             data = new JSONObject();
-            data.put("username","gus@kidozen.com");
-            data.put("password","kidozen*1");
+            data.put("username","your user");
+            data.put("password","your password);
 
             final CountDownLatch signal = new CountDownLatch(2);
             kidozen = new KZApplication(IntegrationTestConfiguration.TENANT, IntegrationTestConfiguration.APP, true, kidoInitCallback(signal));
--- kz.client/src/test/java/IntegrationTestConfiguration.java
+++ kz.client/src/test/java/IntegrationTestConfiguration.java
@@ -2,10 +2,11 @@ public class IntegrationTestConfiguration {
     /*
     * Replace with the right values
     * */
-    public static final String TENANT = "https://armonia.kidocluod.com";
+    public static final String TENANT = "https://armonia.kidocloud.com";
     public static final String APP = "tasks";
     public static final String USR = "armonia@kidozen.com";
 
     public static final String PASS = "pass";
     public static final String PROVIDER = "Kidozen";
 }
+
