3a3311eecd0086111490bbeaa50adf97f255fed4
ccarnero <christian.carnero@gmail.com>
2013-06-21 19:44:50 -0300

    updated tests constants credentials

--- kz.client/src/test/java/IntegrationTestConfiguration.java
+++ kz.client/src/test/java/IntegrationTestConfiguration.java
@@ -2,11 +2,11 @@ public class IntegrationTestConfiguration {
     /*
     * Replace with the right values
     * */
-    public static final String TENANT = "https://armonia.kidocloud.com";
-    public static final String APP = "tasks";
-    public static final String USR = "armonia@kidozen.com";
+    public static final String TENANT = "https://your tenant";
+    public static final String APP = "application name";
+    public static final String USR = "user name @kidozen.com";
 
-    public static final String PASS = "pass";
+    public static final String PASS = "password";
     public static final String PROVIDER = "Kidozen";
 }
 
