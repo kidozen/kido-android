eea9697d604c06840ed5a65b788f32759078fa48
ccarnero <christian.carnero@gmail.com>
2013-06-21 19:36:39 -0300

    updated project file

--- kz.client/src/test/java/IntegrationTestConfiguration.java
+++ kz.client/src/test/java/IntegrationTestConfiguration.java
@@ -2,10 +2,10 @@ public class IntegrationTestConfiguration {
     /*
     * Replace with the right values
     * */
-    public static final String TENANT = "https://your.tenant.com";
-    public static final String APP = "yourapp";
-    public static final String USR = "youruser@kidozen.com";
+    public static final String TENANT = "https://armonia.kidocloud.com";
+    public static final String APP = "tasks";
+    public static final String USR = "armonia@kidozen.com";
 
-    public static final String PASS = "yourpassword";
+    public static final String PASS = "pass";
     public static final String PROVIDER = "Kidozen";
 }
